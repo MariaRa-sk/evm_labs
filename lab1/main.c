461b788279394d87172547cb8ee26cf38488b2d5
MariaRa-sk <m.kuzina@g.nsu.ru>
2025-09-15 23:00:39 +0700

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+double calculate_pi(const size_t N) {
+    double sum = 0;
+    int sign = 1;
+    for (size_t i = 0; i < N; ++i) {
+        sum += (double) sign / (2 * i + 1);
+        sign = -sign;
+    }
+    return 4 * sum;
+}
+
+int main(void) {
+    size_t N = 100;
+    double pi = calculate_pi(N);
+    printf("Число пи: %.10f\n", pi);
+    return 0;
+}
 
